6726cca2df17067348eca9febcf7f8c4b3d38284
Gustavo B. de Souza <114950117+mulack@users.noreply.github.com>
2024-07-14 22:35:36 -0300

    Update product.py

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,12 +1,12 @@
-from typing import List
+from typing import List, Optional
 from uuid import UUID
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 import pymongo
-from store.db.mongo import db_client
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from store.models.product import ProductModel
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 from store.core.exceptions import NotFoundException
-
+from datetime import datetime
+from bson import ObjectId
 
 class ProductUsecase:
     def __init__(self) -> None:
@@ -29,25 +29,41 @@ class ProductUsecase:
         return ProductOut(**result)
 
     async def query(self) -> List[ProductOut]:
-        return [ProductOut(**item) async for item in self.collection.find()]
+        products = []
+        async for item in self.collection.find():
+            products.append(ProductOut(**item))
+        return products
 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
         result = await self.collection.find_one_and_update(
             filter={"id": id},
-            update={"$set": body.model_dump(exclude_none=True)},
+            update={"$set": body.model_dump(exclude_none=True, updated_at=datetime.utcnow())},
             return_document=pymongo.ReturnDocument.AFTER,
         )
+
+        if not result:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
 
         return ProductUpdateOut(**result)
 
     async def delete(self, id: UUID) -> bool:
-        product = await self.collection.find_one({"id": id})
-        if not product:
+        result = await self.collection.delete_one({"id": id})
+        if result.deleted_count == 0:
             raise NotFoundException(message=f"Product not found with filter: {id}")
 
-        result = await self.collection.delete_one({"id": id})
+        return True
 
-        return True if result.deleted_count > 0 else False
+    async def filter_by_price(self, min_price: Optional[float], max_price: Optional[float]) -> List[ProductOut]:
+        filter_query = {}
 
+        if min_price is not None:
+            filter_query["price"] = {"$gt": min_price}
 
-product_usecase = ProductUsecase()
+        if max_price is not None:
+            filter_query["price"] = {"$lt": max_price}
+
+        products = []
+        async for item in self.collection.find(filter_query):
+            products.append(ProductOut(**item))
+        
+        return products
